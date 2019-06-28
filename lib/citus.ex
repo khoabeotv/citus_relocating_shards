fc470797056d5b96e095ce19646c86eca3d7234b
khoabeotv <khoatv141001@gmail.com>
2019-06-28 09:30:51 +0700

    update

--- lib/citus.ex
+++ lib/citus.ex
@@ -4,10 +4,12 @@ defmodule Citus do
   def start_link(), do: DynamicSupervisor.start_link(__MODULE__, [], name: __MODULE__)
   def init(_), do: DynamicSupervisor.init(strategy: :one_for_one)
 
-  def connect() do
+  def connect(opts \\ []) do
     if DynamicSupervisor.count_children(__MODULE__)[:active] == 0 do
       DynamicSupervisor.start_child(__MODULE__, {Citus.Repo, []})
       DynamicSupervisor.start_child(__MODULE__, {Citus.Worker, []})
+
+      if opts[:sub_setup], do: DynamicSupervisor.start_child(__MODULE__, {Citus.SubRepo, []})
     end
   end
 end
--- lib/repo.ex
+++ lib/repo.ex
@@ -1,3 +1,7 @@
 defmodule Citus.Repo do
   use Ecto.Repo, otp_app: :citus, adapter: Ecto.Adapters.Postgres
 end
+
+defmodule Citus.SubRepo do
+  use Ecto.SubRepo, otp_app: :citus, adapter: Ecto.Adapters.Postgres
+end
